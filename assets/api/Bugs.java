f1275955bbfa5ab5ee4bdbc1f42fb2e122b434e5
dxoakes <donald.oakes@centurylink.com>
2017-09-13 17:11:53 -0600

    updated per cookbook

---
+++ assets/api/Bugs.java
@@ -0,0 +1,31 @@
+package api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.Path;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.centurylink.mdw.common.service.ServiceException;
+import com.centurylink.mdw.demo.bugs.Bug;
+import com.centurylink.mdw.model.StatusResponse;
+import com.centurylink.mdw.services.ServiceLocator;
+import com.centurylink.mdw.services.rest.JsonRestService;
+
+import io.swagger.annotations.Api;
+
+@Path("/")
+@Api("Bugs REST API")
+public class Bugs extends JsonRestService {
+
+    @Override
+    public JSONObject post(String path, JSONObject content, Map<String,String> headers)
+            throws ServiceException, JSONException {
+        String requestId = Long.toHexString(System.nanoTime());
+        Object response = ServiceLocator.getWorkflowServices().invokeServiceProcess("Create Bug",
+                new Bug(content), requestId, new HashMap<>(), headers);
+        return ((StatusResponse)response).getJson();
+    }
+}
