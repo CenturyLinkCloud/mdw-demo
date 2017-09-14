e43bd1f23286a556eea4da5e21478a7e7975df3d
donaldoakes <donald.oakes@centurylink.com>
2017-09-14 13:00:08 -0600

    assets per cookbook

--- assets/com/centurylink/mdw/demo/bugs/Bug.java
+++ assets/com/centurylink/mdw/demo/bugs/Bug.java
@@ -1,11 +1,13 @@
 package com.centurylink.mdw.demo.bugs;
     
+import javax.validation.constraints.Size;
+
 import org.json.JSONObject;
 
 import com.centurylink.mdw.model.Jsonable;
 
 import io.swagger.annotations.ApiModelProperty;
-    
+
 public class Bug implements Jsonable {
         
     public Bug(JSONObject json) {
@@ -15,11 +17,13 @@ public class Bug implements Jsonable {
     @ApiModelProperty(readOnly=true)
     private Long id;
     public Long getId() { return id; }
-        
+    
+    @ApiModelProperty(value="Headline bug information", required=true)
+    @Size(max=512)    
     private String title;
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
-        
+
     private String description;
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
