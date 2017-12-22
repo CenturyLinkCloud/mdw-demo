eb84bc93ac82cb78e18772fd3defe76ae8d8f05e
AB59451 <AB59451@5CG7242QK5.ctl.intranet>
2017-12-22 22:53:26 +0530

    Build 6.0.10

--- assets/com/centurylink/mdw/slack/MarkdownScrubber.java
+++ assets/com/centurylink/mdw/slack/MarkdownScrubber.java
@@ -20,31 +20,39 @@ package com.centurylink.mdw.slack;
  * to/from the MDW Discussion tab.  TODO: very crude at the moment
  */
 public class MarkdownScrubber {
-   
+
     private static final String BULLET = String.valueOf((char)8226);
-    
+
     private String input;
     public MarkdownScrubber(String input) {
         this.input = input;
     }
-   
+
     public String toSlack() {
         return input
-        // italics
-        .replaceAll("\\*([^\\s]+?)\\*", "_$1_")
-        // bold
-        .replaceAll("_\\*([^\\s]+?)_\\*", "*$1*")
-        .replaceAll("__([^\\s]+?)__", "*$1*")
-        // strikethrough
-        .replaceAll("~~([^\\s]+?)~~", "~$1~")
-        // unordered lists
-        .replaceAll("(\n\\s*)-(.*)", "$1" + BULLET + "$2")
-        .replaceAll("(\n\\s*)\\*(.*)", "$1" + BULLET + "$2")
-        // links: <http://www.foo.com|www.foo.com>
-        .replaceAll("\\[(.+)]\\((.+)\\)", "<$2|$1>");
+            // italics
+            .replaceAll("\\*([^\\s]+?)\\*", "_$1_")
+            // bold
+            .replaceAll("_\\*([^\\s]+?)_\\*", "*$1*")
+            .replaceAll("__([^\\s]+?)__", "*$1*")
+            // strikethrough
+            .replaceAll("~~([^\\s]+?)~~", "~$1~")
+            // unordered lists
+            .replaceAll("(\n\\s*)-(.*)", "$1" + BULLET + "$2")
+            .replaceAll("(\n\\s*)\\*(.*)", "$1" + BULLET + "$2")
+            // links: <http://www.foo.com|www.foo.com>
+            .replaceAll("\\[(.+)]\\((.+)\\)", "<$2|$1>");
     }
-   
-    private String toMarkdown() {
-        return input; // TODO
+
+    public String toMarkdown() {
+        return input
+            // bold
+            .replaceAll("\\*([^\\s]+?)\\*", "**$1**")
+            // italics
+            .replaceAll("_([^\\s]+?)_", "*$1*")
+            // strikethrough
+            .replaceAll("~([^\\s]+?)~", "~~$1~~")
+            // unordered lists
+            .replaceAll("(\n\\s*)" + BULLET + "(.*)", "$1-$2");
     }
 }
