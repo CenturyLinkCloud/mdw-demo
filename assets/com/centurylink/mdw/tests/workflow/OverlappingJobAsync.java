04743e80398c008222d2aa3dae9477d2bfeaf00b
Donald Oakes <donald.oakes@centurylink.com>
2019-05-13 10:56:44 -0600

    6.1.17

---
+++ assets/com/centurylink/mdw/tests/workflow/OverlappingJobAsync.java
@@ -0,0 +1,21 @@
+package com.centurylink.mdw.tests.workflow;
+
+import com.centurylink.mdw.annotations.ScheduledJob;
+import com.centurylink.mdw.model.monitor.JobCompletionCallback;
+import com.centurylink.mdw.util.CallURL;
+
+/**
+ * Performs work asynchronously in a spawned thread, so uses callback mechanism to notify of job completion.
+ */
+@ScheduledJob(value="OverlappingJobAsync", schedule="* * * * * *",
+        enabled="${props['mdw.overlapping.job.async.enabled']}", isExclusive=true)
+public class OverlappingJobAsync extends OverlappingJob {
+
+    @Override
+    public void run(CallURL args, JobCompletionCallback callback) {
+        new Thread(() -> {
+            doWork();
+            callback.onComplete(0);
+        }).start();
+    }
+}
