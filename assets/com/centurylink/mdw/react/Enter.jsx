d0f130f7ef37cca815c69565bf74e1f50b628ab1
Donald Oakes <donald.oakes@centurylink.com>
2019-11-11 13:06:44 -0700

    upgrade to 6.1.27

--- assets/com/centurylink/mdw/react/Enter.jsx
+++ assets/com/centurylink/mdw/react/Enter.jsx
@@ -7,6 +7,7 @@ class Enter extends Component {
     super(...args);
     this.state = {show: false, message: '', entry: ''};
     this.handleChange = this.handleChange.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   close(result) {
@@ -21,17 +22,39 @@ class Enter extends Component {
   }
 
   handleChange(e) {
-    this.setState({show: this.state.show, message: this.state.message, entry: e.currentTarget.value});
+    let value = e.currentTarget.value;
+    this.setState({show: this.state.show, message: this.state.message, entry: value},
+      () => {
+        if (this.props.onChange) {
+          this.props.onChange(value);
+        }
+    });
+  }
+
+  handleKeyDown(e) {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      e.stopPropagation();
+      this.close(this.state.entry);      
+    }    
   }
 
   render() {
     return (
       <div>
         <Modal show={this.state.show} onHide={() => this.close()}>
-          <Modal.Header closeButton>
-            <Modal.Title>{this.props.title}</Modal.Title>
+          <Modal.Header closeButton 
+            style={{paddingBottom:this.props.error || this.props.error === '' ? '25px' : '15px'}}>
+            <Modal.Title>
+              {this.props.title}
+            </Modal.Title>
+            {this.props.error &&
+              <span className="mdw-warn" style={{position:'absolute'}}>
+                {this.props.error}
+              </span>
+            }
           </Modal.Header>
-          <Modal.Body>
+          <Modal.Body style={{width:'500px'}}>
             <div>
               {this.state.message}
             </div>
@@ -41,16 +64,17 @@ class Enter extends Component {
                   {this.props.label}
                 </ControlLabel>
               }
-              <input type="text" style={{width:'350px',marginTop:'10px'}}
-                value={this.state.entry} 
-                onChange={this.handleChange} />
+              <input type="text" style={{width:'419px',marginTop:'10px'}}
+                value={this.state.entry} autoFocus
+                onChange={this.handleChange}
+                onKeyDown={this.handleKeyDown} />
             </div>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={() => this.close() }>
+            <Button onClick={() => this.close() }>
               Cancel
             </Button>
-            <Button variant="primary" onClick={() => this.close(this.state.entry)}>
+            <Button bsStyle="primary" onClick={() => this.close(this.state.entry)}>
               OK
             </Button>
           </Modal.Footer>
@@ -62,6 +86,8 @@ class Enter extends Component {
 
 Enter.propTypes = {
   title: PropTypes.string.isRequired,
+  error: PropTypes.string,
+  onChange: PropTypes.func,
   onClose: PropTypes.func
 };
 
